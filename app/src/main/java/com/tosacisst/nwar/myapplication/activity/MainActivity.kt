e1958344a349011696d130cf3f97794ab9e28c1f
nvvar <nwar0825@users.noreply.github.com>
2018-12-26 16:12:14 +0900

    뷰 드래그 이동

--- app/src/main/java/com/tosacisst/nwar/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/tosacisst/nwar/myapplication/activity/MainActivity.kt
@@ -2,12 +2,19 @@ package com.tosacisst.nwar.myapplication.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import com.tosacisst.nwar.myapplication.R
+import com.tosacisst.nwar.myapplication.event.SetEvent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val img = findViewById<ImageView>(R.id.img) // 드래그할 뷰
+        img.setOnLongClickListener(SetEvent(this).SetOnLongClick())
+        val layout = findViewById<LinearLayout>(R.id.background) // 드래그한 뷰를 이동시킬 장소.
+        layout.setOnDragListener(SetEvent(this).SetOnDrag())
     }
 }
---
+++ app/src/main/java/com/tosacisst/nwar/myapplication/event/SetEvent.kt
@@ -0,0 +1,49 @@
+package com.tosacisst.nwar.myapplication.event
+
+import android.content.ClipData
+import android.content.ClipDescription
+import android.content.Context
+import android.util.Log
+import android.view.DragEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
+
+class SetEvent(val context : Context){
+    inner class SetOnLongClick : View.OnLongClickListener{
+        override fun onLongClick(v: View?): Boolean {
+            val item = ClipData.Item(v?.tag as CharSequence?)
+            var types : Array<String> = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(v?.tag.toString(),types,item)
+            val shadowBuilder = View.DragShadowBuilder(v)
+            v?.startDragAndDrop(data,shadowBuilder,v,0)
+            v?.visibility = View.VISIBLE
+            return true
+        }
+    }
+
+    inner class SetOnDrag : View.OnDragListener{
+        override fun onDrag(v: View?, event: DragEvent?): Boolean {
+            var x : Float?
+            var y : Float?
+            when (event?.action){
+                DragEvent.ACTION_DRAG_STARTED -> Toast.makeText(context,"드래그", Toast.LENGTH_SHORT).show()
+                DragEvent.ACTION_DROP -> {
+                    val view : View = event.localState as View
+                    val viewGroup : ViewGroup = view.parent as ViewGroup
+                    viewGroup.removeView(view)
+                    val containView : LinearLayout = v as LinearLayout
+                    view.animate().translationX(event?.x-view.width/2)
+                    view.animate().translationY(event?.y-view.height/2)
+                    containView.addView(view)
+                    view.visibility = View.VISIBLE
+                }
+            }
+            x = event?.x
+            y = event?.y
+            Log.e("Drag","Point X: ${x.toString()} PointY: ${y.toString()}")
+            return true
+        }
+    }
+}
